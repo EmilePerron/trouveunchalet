0594f97fba7d42f1a5d3a93f52d0de9002a926b4
Émile Perron <contact@emileperron.com>
2023-09-27 22:07:21 -0400

    feat: allow testing a specific crawler in the CLI

--- src/Command/CrawlCommand.php
+++ src/Command/CrawlCommand.php
@@ -3,9 +3,11 @@
 namespace App\Command;
 
 use App\Crawler\CrawlerRunner;
+use App\Enum\Site;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -23,6 +25,7 @@ class CrawlCommand extends Command
 
     protected function configure(): void
     {
+		$this->addOption('site', 's', InputOption::VALUE_OPTIONAL, "Request to crawl only the specified site.", null);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -30,7 +33,42 @@ class CrawlCommand extends Command
         $io = new SymfonyStyle($input, $output);
 
         set_time_limit(30);
-        $this->crawlerRunner->crawlEverything();
+		$siteOption = $input->getOption("site");
+
+		if (!$siteOption) {
+			$this->crawlerRunner->crawlEverything();
+		} else {
+			$site = Site::tryFrom($siteOption);
+
+			if (!$site) {
+				$mostLikelySite = null;
+				$mostLikelySimilarityPercentage = 0;
+
+				foreach (Site::cases() as $potentialSite) {
+					similar_text($siteOption, $potentialSite->name, $similarityPercentage);
+
+					if ($similarityPercentage > $mostLikelySimilarityPercentage) {
+						$mostLikelySite = $potentialSite;
+						$mostLikelySimilarityPercentage = $similarityPercentage;
+					}
+				}
+
+				if ($mostLikelySite) {
+					$answer = $io->ask("Did you mean {$mostLikelySite->name} ({$mostLikelySite->value}) ? (y/N)");
+
+					if (strtolower(trim($answer)) == "y") {
+						$site = $mostLikelySite;
+					}
+				}
+			}
+
+			if (!$site) {
+				$io->error('No matching site found.');
+				return Command::FAILURE;
+			}
+
+			$this->crawlerRunner->crawlSite($site);
+		}
 
         $io->success('Crawl completed!');
 
