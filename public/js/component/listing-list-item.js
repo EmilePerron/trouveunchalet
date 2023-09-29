d50626ec2622aaebb64edc442cca73516e719fa8
Émile Perron <contact@emileperron.com>
2023-09-29 17:42:14 -0400

    feat: minor improvements to listing UI

--- public/js/component/listing-list-item.js
+++ public/js/component/listing-list-item.js
@@ -4,7 +4,8 @@ const stylesheet = document.createElement("style");
 stylesheet.innerHTML = `
 	:host { display: flex; flex-direction: column; align-items: flex-start; gap: .5rem; background-color: white; position: relative; }
 	.gallery { line-height: 0; border-radius: .75rem; overflow: hidden; }
-	strong { display: block; font-size: 1rem; }
+	strong { display: block; font-size: 1rem; font-weight: 600; }
+	.address { font-size: .85em; color: var(--color-gray-500); }
 	img { width: 100%; aspect-ratio: 4/3; object-fit: cover; object-position: center; border-radius: .25rem; background-color: var(--color-primary-050); }
 
 	.link-overlay { display: grid; place-items: center; width: 100%; aspect-ratio: 4/3; text-decoration: none; background-color: rgb(255 255 255 / 50%); opacity: 0; position: absolute; top: 0; left: 0; transition: opacity .15s ease; }
