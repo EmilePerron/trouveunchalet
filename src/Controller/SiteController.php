fcb6972f03997f3aac656e24d3cd6e7f407829b0
Émile Perron <contact@emileperron.com>
2023-11-17 11:02:49 -0500

    feat: add about page

---
+++ src/Controller/SiteController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\Cache;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Cache(maxage: 3600, public: true, mustRevalidate: true)]
+class SiteController extends AbstractController
+{
+    #[Route('/a-propos', name: 'about')]
+    public function about(): Response
+    {
+        return $this->render('site/about.html.twig');
+    }
+
+    #[Route('/contact', name: 'contact_us')]
+    public function contact(): Response
+    {
+        return $this->render('site/contact.html.twig');
+    }
+}
