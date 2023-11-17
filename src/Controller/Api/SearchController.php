212cbb1df68312d3eb2cce3a9293682eb9cce1d7
Émile Perron <contact@emileperron.com>
2023-11-17 09:59:37 -0500

    perf: add 60s cache to search queries

--- src/Controller/Api/SearchController.php
+++ src/Controller/Api/SearchController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\Cache;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Context\Normalizer\ObjectNormalizerContextBuilder;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -19,6 +20,7 @@ class SearchController extends AbstractController
     ) {
     }
 
+	#[Cache(public: true, maxage: 60, mustRevalidate: true)]
     #[Route('/api/listing/search', name: 'api_listing_search')]
     public function search(Request $request): Response
     {
