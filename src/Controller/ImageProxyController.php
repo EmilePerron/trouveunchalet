6c7b2adb4330094a53ea9d868b915b67303452f5
Émile Perron <contact@emileperron.com>
2023-11-18 10:04:50 -0500

    feat: add image proxy endpoint

---
+++ src/Controller/ImageProxyController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class ImageProxyController extends AbstractController
+{
+	#[Route(path: '/image/listing/{url}', name: 'image_proxy', requirements: ["url" => ".+"])]
+	public function imageProxy(string $url, HttpClientInterface $httpClient): Response
+	{
+		if (!$url) {
+			return new Response("Missing image URL.", 400);
+		}
+
+		$url = urldecode($url);
+
+		$sourceResponse = $httpClient->request("GET", $url, [
+			"timeout" => 2.5,
+			"max_redirects" => 10,
+			"headers" => [
+				"Accept" => "application/json",
+			],
+		]);
+		$statusCode = $sourceResponse->getStatusCode();
+
+		if ($statusCode < 200 || $statusCode >= 300) {
+			return new Response("Could not fetch image", $statusCode);
+		}
+
+		$contentType = $sourceResponse->getHeaders()['content-type'][0];
+
+		if (!str_contains(strtolower($contentType), "image/")) {
+			return new Response("Invalid file type - expecting an image.", 400);
+		}
+
+		$response = new Response($sourceResponse->getContent(), 200, ["Content-Type" => $contentType]);
+		$response->setPublic();
+		$response->setMaxAge(3600);
+
+		return $response;
+	}
+}
